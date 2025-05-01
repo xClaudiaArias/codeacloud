864aa39f86c4e4b78fb5cb5c28240339489700cd
xClaudiaArias <xClaudiaArias@gmail.com>
2025-04-30 21:37:34 -0400

    Initialized project and generate keys

---
+++ personal-blog/src/app/components/blog-card/blog-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-card',
+  imports: [],
+  templateUrl: './blog-card.component.html',
+  styleUrl: './blog-card.component.scss'
+})
+export class BlogCardComponent {
+
+}
---
+++ personal-blog/src/app/pages/blog-detail/blog-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-detail',
+  imports: [],
+  templateUrl: './blog-detail.component.html',
+  styleUrl: './blog-detail.component.scss'
+})
+export class BlogDetailComponent {
+
+}
